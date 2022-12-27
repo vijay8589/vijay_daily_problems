e05803c20cd759c5b01702d267aebaa67097f28c
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-12-27 10:30:23 +0530

    Add files via upload

---
+++ set/discard.py
@@ -0,0 +1,7 @@
+#The discard() method removes the specified item from the set.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
---
+++ set/union.py
@@ -0,0 +1,8 @@
+#Return a set that contains all items from both sets, duplicates are excluded:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.union(y) 
+
+print(z)
